4af6f87a0aee218816b38d4006b4f4c2578b83c9
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-22 19:58:21 -0300

    Test cart system

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import * as cartService from "./services/cart.js";
+import { createItem } from "./services/item.js";
+
 const cart = [];
+
+// Create five items
+const item1 = createItem("Mouse", 20.0, 4);
+const item2 = createItem("Teclado", 50.0, 1);
+const item3 = createItem("Monitor", 350.0, 2);
+const item4 = createItem("I7 2600", 100.0, 5);
+const item5 = createItem("RX 580", 200.0, 3);
+
+cartService.addItemToCart(cart, item1);
+cartService.addItemToCart(cart, item2);
+cartService.addItemToCart(cart, item3);
+cartService.addItemToCart(cart, item4);
+cartService.addItemToCart(cart, item5);
+
+// Remove two units of Mouse
+cartService.removeItem(cart, "Mouse");
+cartService.removeItem(cart, "Mouse");
+
+// Add one unit of Mouse
+cartService.addNewItem(cart, "Mouse");
+
+cartService.displayCart(cart);
+console.log("Total: R$ " + cartService.calcTotalCart(cart).toFixed(2));
